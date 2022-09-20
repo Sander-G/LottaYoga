ebfb86c2e424567eb79638cd9285e8284f16f240
Sander <sander@grunnekemeijer.net>
2022-09-20 19:48:09 +0200

    scroll animation

--- LottaYoga.nl/script.js
+++ LottaYoga.nl/script.js
@@ -1,3 +1,4 @@
+// hamburger
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
 
@@ -5,3 +6,21 @@ menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
+
+// scroll animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
