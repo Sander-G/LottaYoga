1c6bf5ec14340295016496fb6aac46c18d42d030
Sander_iMac <sander@grunnekemeijer.net>
2022-10-06 12:14:12 +0200

    footer fixed at bottom

--- LottaYoga.nl/script.js
+++ LottaYoga.nl/script.js
@@ -8,19 +8,19 @@ menu_btn.addEventListener('click', function () {
 });
 
 // scroll animation
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         }
+//         else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
 
 
